a194f8972d628bf3175d53ab8fa5e90fead5d84b
ashok-4005 <131009403+ashok-4005@users.noreply.github.com>
2025-02-02 13:52:22 +0530

    Add files via upload

---
+++ ecommerceFrontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '20px',
+        backgroundColor: 'black',
+        width: '100%',
+        textAlign: 'center'
+      }}>
+        <p> © AK 2024 All for Learning.</p>
+      </div>
+    );
+  }
